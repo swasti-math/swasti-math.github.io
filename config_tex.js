46daad46615f05880d52749d683beeb03f0996ba
Swasti Tiwari <swasti.stem@gmail.com>
2026-09-16 22:14:21 +0530

    Create config_tex.js

---
+++ config_tex.js
@@ -0,0 +1,10 @@
+// MathJax Configuration
+window.MathJax = {
+  tex: {
+    inlineMath: [['$', '$'], ['\\(', '\\)']],
+    displayMath: [['$$', '$$'], ['\\[', '\\]']]
+  },
+  svg: {
+    fontCache: 'global'
+  }
+};
